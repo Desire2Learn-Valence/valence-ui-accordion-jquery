96e8e52de863513a6381eff8836825d17d0544ad
Dave Batiste <dave.batiste@d2l.com>
2014-10-14 16:44:03 -0400

    Removing vui stuff since it was only needed for the vui.addClassInitializer call.

--- accordion.js
+++ accordion.js
@@ -1,26 +1,8 @@
 /*jslint browser: true*/
 
-(function( vui ) {
+(function() {
 
 	'use strict';
-
-	// Check if the provided vui global is defined, otherwise try to require it if
-	// we're in a CommonJS environment; otherwise we'll just fail out
-	if( vui === undefined ) {
-		if( typeof require === 'function' ) {
-			vui = require('../../core');
-		} else {
-			throw new Error('load vui first');
-		}
-	}
-
-	// Export the vui object if we're in a CommonJS environment.
-	// It will already be on the window otherwise
-	if( typeof module === 'object' && typeof module.exports === 'object' ) {
-		module.exports = vui;
-	}
-
-	var $ = vui.$;
 
 	$.widget( "vui.vui_accordion", {
 
@@ -476,11 +458,4 @@
 
 	} );
 
-	vui.addClassInitializer(
-			'vui-accordion',
-			function( node ) {
-				$( node ).vui_accordion();
-			}
-		);
-
-})( window.vui );
+} )();
