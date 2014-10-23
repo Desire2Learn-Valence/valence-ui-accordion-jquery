090a70b1870b877ac223610ad0d8f71f615d4246
Dave Batiste <dave.batiste@d2l.com>
2014-10-23 15:13:46 -0400

    Converting previous qunit tests to jasmine tests.

--- test/unit/accordionSpec.js
+++ test/unit/accordionSpec.js
@@ -6,9 +6,34 @@
 		var node, accordion, $accordion,
 			header1, heading1, content1,
 			header2, heading2, content2,
-			header3, heading3, content3, toggle3;
+			header3, heading3, content3, toggle3, toggleIcon3;
+
+		var expectExpanded = function( header, content ) {
+
+			expect( header ).toHaveClassName( 'vui-accordion-panel-expanded' );
+			expect( header.getAttribute( 'aria-expanded' ) ).toBe( 'true' );
+			expect( content ).not.toHaveDisplay( 'none' );
+			expect( content.getAttribute( 'aria-hidden' ) ).toBe( 'false' );
+
+		};
+
+		var expectCollapsed = function( header, content ) {
+			expect( header ).not.toHaveClassName( 'vui-accordion-panel-expanded' );
+			expect( header.getAttribute( 'aria-expanded' ) ).toBe( 'false' );
+			expect( content ).toHaveDisplay( 'none' );
+			expect( content.getAttribute( 'aria-hidden' ) ).toBe( 'true' );
+		};
+
+		var createKeyEvent = function( eventType, keyCode ) {
+			var e = $.Event( eventType );
+			e.keyCode = keyCode;
+			return e;
+		};
 
 		beforeEach( function () {
+
+			// turn off jquery fx since they will can introduce timing issues when making our expectations
+			$.fx.off = true;
 
 			jasmine.addMatchers( d2l.jasmine.matchers );
 
@@ -34,6 +59,7 @@
 
 			header2 = accordion.appendChild( document.createElement( 'div' ) );
 			header2.className = 'vui-accordion-header';
+			header2.setAttribute( 'data-content-id', 'content2' );
 			header2.setAttribute( 'data-expanded', 'true' );
 			header2.setAttribute( 'data-content-labelledby', 'heading2' );
 
@@ -42,6 +68,7 @@
 			heading2.innerHTML = 'This is heading 2.';
 
 			content2 = accordion.appendChild( document.createElement( 'div' ) );
+			content2.id = 'content2';
 			content2.className = 'vui-accordion-content';
 
 			content2.appendChild( document.createElement( 'p' ) ).innerHTML = 'This is content 2.';
@@ -53,6 +80,9 @@
 			toggle3 = header3.appendChild( document.createElement( 'div' ) );
 			toggle3.className = 'vui-accordion-toggle';
 
+			toggleIcon3 = toggle3.appendChild( document.createElement( 'div' ) );
+			toggleIcon3.className = 'vui-accordion-toggle-icon';
+
 			heading3 = header3.appendChild( document.createElement( 'h2' ) );
 			heading3.id = 'heading3';
 			heading3.innerHTML = 'This is heading 3.';
@@ -61,8 +91,6 @@
 			content3.className = 'vui-accordion-content';
 
 			content3.appendChild( document.createElement( 'p' ) ).innerHTML = 'This is content 3.';
-
-			$accordion = $( accordion ).vui_accordion();
 
 		} );
 
@@ -80,11 +108,15 @@
 
 		describe( 'create', function() {
 
+			beforeEach( function() {
+				$accordion = $( accordion ).vui_accordion();
+			} );
+
 			it( 'binds the accordion elements using widget method', function() {
 				expect( $accordion.data( 'vui-vui_accordion' ) ).toBeDefined();
 			} );
 
-			it( 'sets aria attributes', function() {
+			it( 'sets accordion to have tablist aria role', function() {
 				expect( accordion.getAttribute( 'role' ) ).toBe( 'tablist' );
 			} );
 
@@ -126,6 +158,7 @@
 		describe( 'destroy', function() {
 
 			beforeEach( function () {
+				$accordion = $( accordion ).vui_accordion();
 				$accordion.vui_accordion( 'destroy' );
 			} );
 
@@ -143,6 +176,320 @@
 			it( 'removes tab-index from headers', function() {
 				expect( header1.getAttribute( 'tabindex' ) ).toBeNull();
 				expect( header2.getAttribute( 'tabindex' ) ).toBeNull();
+			} );
+
+		} );
+
+		describe( 'any mode', function() {
+
+			beforeEach( function () {
+				$accordion = $( accordion ).vui_accordion();
+			} );
+
+			describe( 'expandPanel event', function() {
+
+				it( 'is triggered with isFirstExpand (true) for heading1 when header is expanded for the first time', function( done ) {
+
+					$accordion.on( 'expandPanel', function( e, args ) {
+						if ( args.header.getAttribute( 'data-content-labelledby' ) !== 'heading1' ) {
+							return;
+						}
+						expect( args.isFirstExpand ).toBeTruthy();
+						done();
+					} );
+
+					$( header1 ).click();
+
+				} );
+
+				it( 'is triggered with isFirstExpand (false) for heading1 when header is expanded a second time', function( done ) {
+
+					var expandCount = 0;
+
+					$accordion.on( 'expandPanel', function( e, args ) {
+						if ( args.header.getAttribute( 'data-content-labelledby' ) !== 'heading1' ) {
+							return;
+						}
+						expandCount += 1;
+						if ( expandCount === 2 ) {
+							expect( args.isFirstExpand ).toBeFalsy();
+							done();
+						}
+					} );
+
+					$( header1 ).click(); // expand 1
+					$( header1 ).click(); // collapse
+					$( header1 ).click(); // expand 2
+
+				} );
+
+			} );
+
+			describe( 'collapsePanel event', function() {
+
+				it( 'is triggered for header2 when header1 is expanded', function( done ) {
+
+					$accordion.on( 'collapsePanel', function( e, args ) {
+						if ( args.header.getAttribute( 'data-content-labelledby' ) !== 'heading2' ) {
+							return;
+						}
+						done();
+					} );
+
+					$( header1 ).click();
+
+				} );
+
+			} );
+
+			describe( 'active header/panel', function() {
+
+				it( 'right arrow key sets next header to be active', function() {
+
+					header1.focus();
+
+					$( header1 ).trigger( createKeyEvent( 'keyup', 39 ) );
+
+					expect( header2 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header1 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'right arrow key sets first header to be active when last header is active', function() {
+
+					header3.focus();
+
+					$( header3 ).trigger( createKeyEvent( 'keyup', 39 ) );
+
+					expect( header1 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header3 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'down arrow key sets next header to be active', function() {
+
+					header1.focus();
+
+					$( header1 ).trigger( createKeyEvent( 'keyup', 40 ) );
+
+					expect( header2 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header1 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'left arrow key sets previous header to be active', function() {
+
+					header2.focus();
+
+					$( header2 ).trigger( createKeyEvent( 'keyup', 37 ) );
+
+					expect( header1 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header2 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'left arrow key sets last header to be active when first header is active', function() {
+
+					header1.focus();
+
+					$( header1 ).trigger( createKeyEvent( 'keyup', 37 ) );
+
+					expect( header3 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header1 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'up arrow key sets previous header to be active', function() {
+
+					header2.focus();
+
+					$( header2 ).trigger( createKeyEvent( 'keyup', 38 ) );
+
+					expect( header1 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header2 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'end arrow key sets last header to be active', function() {
+
+					header1.focus();
+
+					$( header1 ).trigger( createKeyEvent( 'keyup', 35 ) );
+
+					expect( header1 ).not.toHaveClassName( 'vui-accordion-header-active' );
+					expect( header3 ).toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+				it( 'home arrow key sets last header to be active', function() {
+
+					header3.focus();
+
+					$( header3 ).trigger( createKeyEvent( 'keyup', 36 ) );
+
+					expect( header1 ).toHaveClassName( 'vui-accordion-header-active' );
+					expect( header3 ).not.toHaveClassName( 'vui-accordion-header-active' );
+
+				} );
+
+			} );
+
+			describe( 'custom header', function() {
+
+				it( 'has correct icon class when expanded/collapsed', function() {
+					$( header3 ).click();
+					expect( toggleIcon3 ).toHaveClassName( 'vui-icon-accordion-expanded' );
+					expect( toggleIcon3 ).not.toHaveClassName( 'vui-icon-accordion-collapsed' );
+					$( header3 ).click();
+					expect( toggleIcon3 ).not.toHaveClassName( 'vui-icon-accordion-expanded' );
+					expect( toggleIcon3 ).toHaveClassName( 'vui-icon-accordion-collapsed' );
+				} );
+
+			} );
+
+		} );
+
+		describe( 'accordion mode', function() {
+
+			beforeEach( function () {
+				$accordion = $( accordion ).vui_accordion();
+			} );
+
+			describe( 'create', function() {
+
+				it( 'sets accordion to have aria-multiselectable to be false', function() {
+					expect( accordion.getAttribute( 'aria-multiselectable' ) ).toBe( 'false' );
+				} );
+
+			} );
+
+			describe( 'expand/collapse via click', function() {
+
+				it( 'expands content1 when header1 is clicked', function() {
+					$( header1 ).click();
+					expectExpanded( header1, content1 );
+				} );
+
+				it( 'collapses content2 when header1 is clicked', function() {
+					$( header1 ).click();
+					expectCollapsed( header2, content2 );
+				} );
+
+				it( 'collapses content1 when header1 is clicked if already expanded', function() {
+					$( header1 ).click();
+					$( header1 ).click();
+					expectCollapsed( header1, content1 );
+				} );
+
+			} );
+
+			describe( 'expand/collapse via enter', function() {
+
+				it( 'expands content1 when [enter] key is pressed while focus is on header1', function() {
+
+					var e = createKeyEvent( 'keyup', 13 );
+
+					header1.focus();
+
+					$( header1 ).trigger( e );
+					expectExpanded( header1, content1 );
+
+					$( header1 ).trigger( e );
+					expectCollapsed( header1, content1 );
+
+				} );
+
+			} );
+
+			describe( 'expand/collapse via space', function() {
+
+				it( 'expands content1 when [space] key is pressed while focus is on header1', function() {
+
+					var e = createKeyEvent( 'keyup', 32 );
+
+					header1.focus();
+
+					$( header1 ).trigger( e );
+					expectExpanded( header1, content1 );
+
+					$( header1 ).trigger( e );
+					expectCollapsed( header1, content1 );
+
+				} );
+
+			} );
+
+		} );
+
+		describe( 'stacked mode', function() {
+
+			beforeEach( function () {
+				accordion.setAttribute( 'data-display-mode', 'stacked' );
+				$accordion = $( accordion ).vui_accordion();
+			} );
+
+			describe( 'create', function() {
+
+				it( 'sets accordion to have aria-multiselectable to be false', function() {
+					expect( accordion.getAttribute( 'aria-multiselectable' ) ).toBe( 'true' );
+				} );
+
+			} );
+
+			describe( 'collapseAll', function() {
+
+				it( 'collapses all expanded panels', function() {
+
+					$( header1 ).click();
+
+					expectExpanded( header1, content1 );
+					expectExpanded( header2, content2 );
+
+					$( accordion ).vui_accordion( 'collapseAll' );
+
+					expectCollapsed( header1, content1 );
+					expectCollapsed( header2, content2 );
+
+				} );
+
+			} );
+
+			describe( 'expand/collapse via click', function() {
+
+				it( 'leaves content2 expanded when header1 is clicked to expand content1', function() {
+
+					$( header1 ).click();
+
+					expectExpanded( header1, content1 );
+					expectExpanded( header2, content2 );
+
+				} );
+
+			} );
+
+		} );
+
+		describe( 'disabled/empty content', function() {
+
+			beforeEach( function () {
+				content1.innerHTML = '';
+				$accordion = $( accordion ).vui_accordion();
+			} );
+
+			it( 'is styled to be disabled', function() {
+				expect( header1 ).toHaveClassName( 'vui-accordion-header-disabled' );
+			} );
+
+			it( 'does not expand when disabled (content element is empty)', function() {
+
+				header1.focus();
+
+				expectCollapsed( header1, content1 );
+
+				$( header1 ).trigger( createKeyEvent( 'keyup', 13 ) );
+
+				expectCollapsed( header1, content1 );
+
 			} );
 
 		} );
